4fbed8a4569fea245f00607100e8bd4afc83b51f
JohnEdrickGador <jmgador@up.edu.ph>
2024-10-20 22:47:05 +0800

    add: added email field to wallet model

--- backend/dws_api/dws_api/models.py
+++ backend/dws_api/dws_api/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Wallet(models.Model):
     first_name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 200)
     balance = models.FloatField()
     date_created = models.DateField(auto_now_add = True)
