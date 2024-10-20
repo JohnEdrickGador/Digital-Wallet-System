1bfe0e0af4be126499d9c5a6906f7d6c93c6ece6
JohnEdrickGador <jmgador@up.edu.ph>
2024-10-20 22:38:55 +0800

    add: created a Wallet class

---
+++ backend/dws_api/dws_api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Wallet(models.Model):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 50)
+    balance = models.FloatField()
+    account_created = models.DateField(auto_now_add = True)
