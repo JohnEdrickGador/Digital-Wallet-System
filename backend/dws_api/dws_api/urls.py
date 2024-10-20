55d06e89b247de46ca0251ee4651a4967b13793e
JohnEdrickGador <jmgador@up.edu.ph>
2024-10-20 22:55:36 +0800

    feat: created endpoint for wallet creation

--- backend/dws_api/dws_api/urls.py
+++ backend/dws_api/dws_api/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dws_api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create_account/', views.create_wallet)
 ]
