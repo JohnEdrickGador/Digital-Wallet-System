ff050e86642d172946292a03364095eedba96566
JohnEdrickGador <jmgador@up.edu.ph>
2024-10-21 23:03:44 +0800

    feat: created initial UI for login page

--- frontend/dws_frontend/src/pages/Login.jsx
+++ frontend/dws_frontend/src/pages/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='login-container'>
+        <div className="title-container">
+            <h2>Digital Wallet System</h2>
+        </div>
+        <TextField id="outlined-basic" label="Email Address" variant="outlined" />
+        <TextField id="outlined-basic" label="Password" variant="outlined" type='password'/>
+        <div className="login-btn-container">
+            <Button variant="contained">Sign in</Button>
+            <Button variant="outlined">Create an Account</Button>
+        </div>
+    </div>
   )
 }
