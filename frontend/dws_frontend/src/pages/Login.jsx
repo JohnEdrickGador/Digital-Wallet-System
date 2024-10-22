65eac25688e7f4c175816b0f80e9b89e87493cb2
JohnEdrickGador <jmgador@up.edu.ph>
2024-10-22 11:03:55 +0800

    add: connected the login page to the login function of the backend

--- frontend/dws_frontend/src/pages/Login.jsx
+++ frontend/dws_frontend/src/pages/Login.jsx
@@ -1,19 +1,66 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const emailChangeHandler = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    if (!email || !password) {
+      alert("Please enter an email address and a password");
+      return;
+    }
+
+    const requestBody = {
+      "email": email,
+      "password": password
+    }
+
+    const loginEndpoint = 'http://127.0.0.1:8000/login/'
+
+    try {
+      const response = await fetch(loginEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': "application/json"
+        },
+        body: JSON.stringify(requestBody)
+      }
+    )
+
+    if (!response.body) {
+      console.log(response)
+    }
+
+    console.log(response)
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
   return (
-    <div className='login-container'>
+    <form className='login-container' onSubmit={handleLogin}>
         <div className="title-container">
             <h2>Digital Wallet System</h2>
         </div>
-        <TextField id="outlined-basic" label="Email Address" variant="outlined" />
-        <TextField id="outlined-basic" label="Password" variant="outlined" type='password'/>
+        <TextField id="outlined-basic" label="Email Address" variant="outlined" onChange={emailChangeHandler}/>
+        <TextField id="outlined-basic" label="Password" variant="outlined" type='password' onChange={passwordChangeHandler}/>
         <div className="login-btn-container">
-            <Button variant="contained">Sign in</Button>
+            <Button variant="contained" type='submit'>Sign in</Button>
             <Button variant="outlined">Create an Account</Button>
         </div>
-    </div>
+    </form>
   )
 }
